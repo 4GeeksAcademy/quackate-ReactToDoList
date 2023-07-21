bf5864d372c0bab15f3974887032ea80335a2494
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-07-21 01:29:57 +0000

    Project Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [newTodo, setNewTodo] = useState("");
+	const [list, setList] = useState(["Make the bed", "Wash my hands", "Brush my teeth"]);
+
+
+	const keyPress = (e) => {
+		if (e.key === "Enter"){
+			setList(list.concat(newTodo))
+			setNewTodo("");
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To Do List &#x2705;&#x1F41D;</h1>
+			<div className="listWrapper">
+				<ul>
+					<li className="item px-5">
+						<input type="text" onChange={e => setNewTodo(e.target.value)} value={newTodo} onKeyDown={keyPress} id="add-todo" placeholder="What needs to be done?" />
+					</li>
+
+					{list.map((l, i) => (
+						<li className=" item px-5 d-flex">{l}
+						<div className="trash">
+							<i className="far fa-trash-alt" onClick={() => setList(list.filter((t, currentIndex) => i != currentIndex))}></i>
+						</div>
+						</li>
+					))}
+
+					<li className="px-4 tasks-left">
+						{list.length} tasks {list.length === 0 ? ", add a task!" : ""}
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
